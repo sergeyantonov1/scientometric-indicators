575c7ed4a31d0967a11bf4b72fcff7a149c5c91d
sergeyantonov1 <sergey.antonov@flatstack.com>
2019-04-04 16:26:49 +0300

    Wip

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,16 +2,27 @@ class DashboardController < ApplicationController
   expose :selectable_authors, :fetch_selectable_authors
 
   def index
-    @result = Chart::FetchDatasets.call(
-      start_date: params[:start_date],
-      end_date: params[:end_date] || "2010",
-      author_ids: params[:author_ids] || "2019"
-    )
+    if dataset_params?
+      @result = Chart::FetchDatasets.call(
+        start_date: dashboard_params[:start_date],
+        end_date: dashboard_params[:end_date],
+        author_ids: dashboard_params[:author_ids],
+        profile_type: dashboard_params[:profile_type]
+      )
+    end
   end
 
   private
 
+  def dashboard_params
+    params[:dashboard]
+  end
+
   def fetch_selectable_authors
     Author.all.map { |a| [a.full_name, a.id] }
+  end
+
+  def dataset_params?
+    dashboard_params[:start_date] && dashboard_params[:end_date] && dashboard_params[:author_ids] && dashboard_params[:profile_type]
   end
 end
--- app/interactors/chart/generate_datasets.rb
+++ app/interactors/chart/generate_datasets.rb
@@ -2,56 +2,55 @@ module Chart
   class GenerateDatasets
     include Interactor
 
-    delegate :raw_datasets, :labels, to: :context
+    delegate :datasets, :labels, :profile_type, to: :context
 
     def call
       authors = Author.where(id: author_ids)
+      datasets = []
 
       authors.each do |author|
-        pr = author.profiles.where(profile_type: "scopus")&.first
-
-        generate_dataset(pr)
+        pr = author.profiles.where(profile_type: profile_type)&.first
+        byebug
+        generate_datasets(pr) if pr
       end
-
-
-      context.datasets = handle_datasets
     end
 
     private
 
-    def generate_dataset(profile)
-      profile.publications.each do |publiction|
-        labels.each do |label|
-        end
+    def author_ids
+      @author_ids ||= context.author_ids.delete("")
+    end
+
+    def generate_datasets(profile)
+      datas = generate_data(profile)
+
+      %i[publications citations].each do |type|
+        datasets << generate_dataset(profile.author.full_name, datas[type], type)
       end
     end
 
-    def handle_datasets
-      datasets = []
-
-      raw_datasets.each do |r_d|
-        color = random_color
-
-        datasets << generate_dataset(raw_dataset: r_d, type: :citations, color: color)
-        datasets << generate_dataset(raw_dataset: r_d, type: :publications, color: color)
-      end
-
-      datasets
-    end
-
-    def generate_dataset(raw_dataset:, type:, color:)
-      data = []
-
-      labels.each do |label|
-        data << raw_dataset[label][type]
-      end
-
+    def generate_dataset(author:, data:, type:)
       {
-        label: "#{raw_dataset[:full_name]} #{type}",
+        label: "#{profile.author.full_name} #{type}",
         data: data,
         type: type == :citations ? "line" : "bar",
         borderColor: color
       }
+    end
+
+    def generate_data(profile)
+      publications = profile.publications.where(year: labels)
+      publications_count = []
+      citations_count = []
+
+      labels.each do |label|
+        pub = publications.find_by(year: label)
+
+        publications_count << pub&.publications_count || 0
+        citations_count << pub&.citations_count || 0
+      end
+
+      { publications: publications_count, citations: citations_count }
     end
 
     def random_color
