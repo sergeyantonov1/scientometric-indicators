3878ae2e9d70c283077c57f91f3227f073e1c518
sergeyantonov1 <sergey.antonov@flatstack.com>
2019-05-14 18:36:48 +0300

    Fix

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -1,14 +1,20 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+var chart = document.getElementById('myChart')
 
-var mixedChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    datasets: window.__DATA__.datasets,
-    labels: window.__DATA__.labels,
-  },
-  options: {
-    tooltips: {
-      mode: 'point'
+if (typeof(chart) != 'undefined' && chart != null)
+{
+  var ctx = chart.getContext('2d');
+
+  var mixedChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      datasets: window.__DATA__.datasets,
+      labels: window.__DATA__.labels,
     },
-  }
-});
+    options: {
+      tooltips: {
+        mode: 'point'
+      },
+    }
+  });
+}
+
